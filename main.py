97f1cb14c384a1921199c56dca124ab9e2a59e75
tamjidzihan <tamjidzihan@gmail.com>
2022-08-23 21:00:13 +0600

    Office 23-08-2022

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-from re import I
-from turtle import st
-
-
 class Item:
-    '''
-    Product Price calculator
-    '''
+    pay_rate = 0.8
+    all = []
     def __init__(self,name:str, price:float,quantity=0) -> None:
+        
+        assert price >=1, f"Price {price} is not greater then zero!"
+        assert quantity >=1
+
+
         self.name = name
         self.price = price
         self.quantity = quantity
 
+        Item.all.append(self)
+
     def calculate_total_price(self):
         return  print(self.price * self.quantity)
 
-item1 = Item('Phone',300,4)
-item1.calculate_total_price()
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+
+item1 = Item("Phone", 100, 1)
+item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
+
+print(Item.all)
